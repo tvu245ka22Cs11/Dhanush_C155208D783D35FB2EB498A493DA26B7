1367098f84c3697265e4a79866aedca1b87d5f4c
tvu245ka22Cs11 <144468762+tvu245ka22Cs11@users.noreply.github.com>
2023-10-19 10:09:45 +0530

    Add files via upload

--- main-1.py
+++ main-1.py
@@ -1,11 +1,11 @@
-P = int( input("Please enter value for P: "))  
-Q = int( input("Please enter value for Q: "))  
-   
-# To swap the value of two variables  
-# we will user third variable which is a temporary variable  
-temp_1 = P  
-P = Q  
-Q = temp_1  
-   
-print ("The Value of P after swapping: ", P)  
-print ("The Value of Q after swapping: ", Q)
+def linear_search_product(productlist,targetproduct):
+    indices=[]
+    for index,product in enumerate(productlist):
+ 
+        if product== targetproduct:
+            indices.append(index)
+        return indices
+products=['pen','pencil','paper','eraser','book','sharpner'] 
+target=input("Enter the element to be searched: ")  
+result=linear_search_product(products, target)
+print("The index of ",target," is" ,result)          
